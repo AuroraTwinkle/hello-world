ce8a15dc9f1aed9ad3491b39b36faf2ad39190a3
xiaoyuann <602632139@qq.com>
2017-02-25 14:22:19 +0800

    输出字节长度

---
+++ 初学/字节长度.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("sizeof(float)=%d\n",sizeof(float));
+    printf("sizeof(double)=%d\n",sizeof(double));
+    printf("sizeof(long double)=%d\n",sizeof(long double));
+
+    return 0;
+}
