474668d764a8580739e53377ae213cb16fd53b8d
xiaoyuann <602632139@qq.com>
2017-02-25 15:47:09 +0800

    字符变量

---
+++ 初学/字符变量.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    /* 定义并初始化各种字符变量 */
+    char c1 = '\n';
+    unsigned char c2 = '\t';
+    char c3 = 49;
+
+    printf("c1 = [%c],[%d]\n",c1,c1);
+    printf("c2 = [%c],[%d]\n",c2,c2);
+    printf("c3 = [%c],[%d]\n",c3,c3);
+
+    return 0;
+}
