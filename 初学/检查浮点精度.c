1f79637e97e9bd4edbd25e2cefb9111bb017732a
xiaoyuann <602632139@qq.com>
2017-02-25 22:27:47 +0800

    浮点精度

---
+++ 初学/检查浮点精度.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float f = 1.256486555445774547f;
+    double d= 2.68535685655;
+
+    printf("f\t=%.10f\n",f);
+    printf("d\t=%.10lf\n",d);
+
+    return 0;
+}
