e937dac29115bd4774edb9ba239af11cfe03f290
xiaoyuann <602632139@qq.com>
2017-11-12 17:32:48 +0800

    1

--- c basic/homework/contact.c
+++ c basic/homework/contact.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 void Guide();
-void BuildContact();
 void AddContact();
 void ExportContact();
 void SearchContact();
@@ -10,6 +9,15 @@ void NewContact();
 void ModifyContact();
 void DeleteContact();
 void ShowContact();
+
+typedef struct contact
+{
+	char name[20];
+	char phone[15];
+	char address[50];
+}Contact;
+
+Contact contact[1009];
 
 int main(void)
 {
@@ -26,7 +34,7 @@ int main(void)
 		switch (choice)
 		{
 		case 1:
-			BuildContact();
+			ShowContact();
 			break;
 		case 2:
 			AddContact();
@@ -46,12 +54,9 @@ int main(void)
 		case 7:
 			DeleteContact();
 			break;
-		case 8:
-			ShowContact();
-			break;
 		default:;
 		}
-		if (choice == 9)
+		if (choice == 8)
 			break;
 	}
 }
@@ -99,7 +104,7 @@ void SearchContact()
 void Guide()
 {
 	printf("what do you want to do?\n");
-	printf("1.build a contact\n2.add from file\n3.export to file\n");
+	printf("1.show all contacts\n2.add from file\n3.export to file\n");
 	printf("4.search for contact\n5.build a new contact\n6.modify contact\n");
-	printf("7.delete contact\n8.show all contacts\n9.exit\n");
+	printf("7.delete contact\n8.exit\n");
 }
